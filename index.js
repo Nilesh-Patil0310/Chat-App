5a1def51dd16e038de34d453eebf4257413bb1a5
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2024-05-11 00:20:42 +0530

    db conection and create required schemas

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 import { Server } from "socket.io";
 
-import cors from 'cors';
-import http, { createServer } from 'http';
+import cors from "cors";
+import http, { createServer } from "http";
+import { connect } from "./config.js";
+import { chatModel } from "./chat.Schema.js";
 
 const app = express();
 
@@ -13,28 +15,44 @@ const server = http.createServer(app);
 
 // 2. create socket server
 
-const io = new Server(server,{
-    cors:{
-        origin:'*',
-        methods:['GET','POST']
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 // 3. use Socket events
 
-io.on('connection', (socket)=>{
-    console.log("connection is establish");
+io.on("connection", (socket) => {
+  console.log("connection is establish");
 
-    socket.on('new_message', (message)=>{
-        // broadcast the message to all clients
-        socket.broadcast.emit('broadcast_message',message);
-    })
+  socket.on("join", (data) => {
+    socket.username = data;
+  });
 
-    socket.on('disconnect', ()=>{
-        console.log("Connection is disconnect")
-    })
+  socket.on("new_message", (message) => {
+    let userMessage = {
+      username: socket.username,
+      message: message,
+    };
+
+    const newChat = new chatModel({
+        username:socket.username,
+        message:message,
+        timestamp:new Date()
+    });
+    newChat.save();
+    // broadcast the message to all clients
+    socket.broadcast.emit("broadcast_message", userMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Connection is disconnect");
+  });
 });
 
-server.listen(3000, ()=>{
-    console.log("App is listening on 3000")
-})
+server.listen(3000, () => {
+  console.log("App is listening on 3000");
+  connect();
+});
